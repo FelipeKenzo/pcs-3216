bc9bc00b84e3f3c78696bd543a1844e750814290
felipekenzo <felipe.kenzo@live.com>
2020-04-28 20:09:57 -0300

    Basic VNM implemented. Needs testing.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <iostream>
+#include <string>
+
+#include "../include/VonNeumannMachine.h"
+
+
+int main (int argc, char* argv[]) {
+
+    VonNeumannMachine mvn = VonNeumannMachine();
+
+    return 0;
+}
