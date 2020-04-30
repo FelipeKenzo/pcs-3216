196b6f89f747b6dd82397baa2ac50128662d7973
felipekenzo <felipe.kenzo@live.com>
2020-04-30 00:32:01 -0300

    Reorganization

--- Assembler/include/DataArea.h
+++ Assembler/include/DataArea.h
@@ -2,7 +2,7 @@
 #define DATAAREA_H
 
 #include <cstdint>
-#include <unordered_map>
+#include <map>
 
 enum type {
     instruction = 0,
@@ -16,37 +16,6 @@ struct mnemonicData {
     bool param;     // If requires parameter
 };
 
-// Instructions
-mnemonicData jp  = {0x0, instruction, 2, true};
-mnemonicData jz  = {0x1, instruction, 2, true};
-mnemonicData jne = {0x2, instruction, 2, true};
-mnemonicData lv  = {0x3, instruction, 2, true};
-mnemonicData add = {0x4, instruction, 2, true};
-mnemonicData sub = {0x5, instruction, 2, true};
-mnemonicData mul = {0x6, instruction, 2, true};
-mnemonicData dv  = {0x7, instruction, 2, true};
-mnemonicData ld  = {0x8, instruction, 2, true};
-mnemonicData mm  = {0x9, instruction, 2, true};
-mnemonicData sc  = {0xA, instruction, 2, true};
-mnemonicData rs  = {0xB, instruction, 2, true};
-mnemonicData hm  = {0xC, instruction, 2, true};
-mnemonicData gd  = {0xD, instruction, 2, true};
-mnemonicData pd  = {0xE, instruction, 2, true};
-mnemonicData os  = {0xF, instruction, 2, true};
-
-// Pseudoinstructions
-mnemonicData org = {16, pseudo, 0, true };
-mnemonicData end = {16, pseudo, 0, false};
-mnemonicData con = {16, pseudo, 2, true };
-
-static std::map<std::string, mnemonicData> mTable = {
-    {"JP" ,  jp}, {"JZ" ,  jz}, {"JN" , jne}, {"LV" ,  lv},
-    {"ADD", add}, {"SUB", sub}, {"MUL", mul}, {"DIV",  dv},
-    {"LD" ,  ld}, {"MM" ,  mm}, {"SC" ,  sc}, {"RS" ,  jp},
-    {"HM" ,  hm}, {"GD" ,  gd}, {"PD" ,  pd}, {"OS" ,  os},
-    {"ORG", org}, {"END", end}, {"CON", con}
-};
-
 struct symbolData {
     uint16_t address;
     uint16_t line;
@@ -54,6 +23,33 @@ struct symbolData {
     bool isReferenced;
 };
 
-static std::map<std::string, symbolData> sTable;
+// Instructions
+// mnemonicData jp  = {};
+// mnemonicData jz  = {};
+// mnemonicData jne = {};
+// mnemonicData lv  = {};
+// mnemonicData add = {};
+// mnemonicData sub = {};
+// mnemonicData mul = {};
+// mnemonicData dv  = {};
+// mnemonicData ld  = {};
+// mnemonicData mm  = {};
+// mnemonicData sc  = {};
+// mnemonicData rs  = {};
+// mnemonicData hm  = {};
+// mnemonicData gd  = {};
+// mnemonicData pd  = {};
+// mnemonicData os  = {};
+
+// Pseudoinstructions
+// mnemonicData org;
+// mnemonicData end;
+// mnemonicData con;
+
+// Mnemonic Table
+// std::map<std::string, mnemonicData> mTable;
+
+// Symbol Table
+
 
 #endif
--- Assembler/src/main.cpp
+++ Assembler/src/main.cpp
@@ -7,17 +7,10 @@
 #include <regex>
 
 #include "../include/DataArea.h"
+#include "../include/Functions.h"
 
-// Function Declarations
-static void printUsage();
-static void printSTable();
-static std::string uppercase(const std::string& s);
-static std::uint16_t htoi(const std::string& hex);
-static int16_t pow16 (int16_t exp);
-static std::vector<std::string> split(const std::string& str, char delim); // Taken from github (https://stackoverflow.com/questions/5607589/right-way-to-split-an-stdstring-into-a-vectorstring)
-static bool isValidLabel(const std::string& s);
-static bool isValidMnemonic(const std::string& s);
-static bool isNumber(const std::string& s); 
+extern std::map<std::string, mnemonicData> mTable;
+extern std::map<std::string, symbolData> sTable;
 
 // Main
 int main(int argc, char* argv[]) {
@@ -72,10 +65,12 @@ int main(int argc, char* argv[]) {
     
     bool isEnd = false;               // Program End flag
     bool isError = false;             // Error flag
+    bool hasLabel = false;
 
     /*---- First Pass -> Generate Symbol and Constant Tables, report syntax errors. ----*/
     while (getline(src, line) && !isEnd) {
         isEnd = false;
+        hasLabel = false;
         lc++;
 
         // Line pre-processing:
@@ -94,6 +89,7 @@ int main(int argc, char* argv[]) {
 
         // If more than two words, first is a label:
         if (words.size() > 2) {
+            hasLabel = true;
             std::string newLabel = words[0];
             //std::cout << newLabel << "\n";
 
@@ -141,12 +137,22 @@ int main(int argc, char* argv[]) {
         // Check if instruction is at valid address
         if (mTable[mnemonic].size > 0) {
             if (pc > 0xFFF) {
-                std::cerr <<"Error(" << lc << "): invalid instruction address (0x" << std::hex << pc << ") at line (" << std::dec << lc << ").\n";
+                std::cerr <<"Error(" << lc << "): invalid instruction address (0x" << std::hex << pc << ").\n";
                 continue;
             }
         }
 
-        if (mnemonic == "END") isEnd = true; // Important to end step 1.
+        if (mnemonic == "END") {
+            isEnd = true; // Important to end step 1.
+            if (hasLabel) {
+                std::cerr <<"Error(" << lc << "): END cannot have label.\n";
+                isError = true;
+            }
+        }
+        if (mnemonic == "ORG" && hasLabel) {
+            std::cerr <<"Error(" << lc << "): ORG cannot have label.\n";
+            isError = true;
+        }
 
         // Does it require an operand?
         if(mTable[mnemonic].param) {
@@ -310,6 +316,8 @@ int main(int argc, char* argv[]) {
             else pc = std::stoi(operand);              // operand in DEC
         }
 
+        if (mnemonic == "END") isEnd = true;
+
         // Calculate machine code
         if (mTable[mnemonic].size == 2) {
             code = mTable[mnemonic].opcode << 12;
@@ -342,7 +350,7 @@ int main(int argc, char* argv[]) {
         }
         
         // End block and write to vnc file
-        if ((size == 16 || mnemonic == "ORG" || mnemonic == "END") && addr) {
+        if ((size == 16 || mnemonic == "ORG" || isEnd) && addr) {
             //std::cout << "ENTRI\n";
             //std::cout << size << "\n";
             vnc << std::right << std::setfill('0') <<  std::uppercase
@@ -365,8 +373,7 @@ int main(int argc, char* argv[]) {
                 data = data >> 4;
             }
             
-
-            std::cout << std::hex << chks << "\n";
+            //std::cout << std::hex << chks << "\n";
 
             // One's complement
             chks = 0xFFFF - chks;
@@ -420,124 +427,3 @@ int main(int argc, char* argv[]) {
     return(0);
 }
 
-//*----------------------------------*//
-//*---- Function Implementations ----*//
-//*----------------------------------*//
-
-static void printUsage() {
-    std::cerr << "Usage: assembler <option(s)> source\n\n"
-              << "Options: -o <destination>"
-              << std::endl;
-}
-
-// Prints constructed symbol table
-static void printSTable() {
-    std::cout << "Symbol Table:\n";
-    
-    std::cout << std::internal
-              << std::setfill('0');
-    
-    std::for_each(sTable.begin(), sTable.end(),
-        [](std::pair<std::string, symbolData > element)
-        {
-            std::cout << "0x" << std::setw(3) <<  std::hex << element.second.address << ": " << element.first << "\n";
-        });
-    std::cout << "\n";
-}
-
-// Converts given string to uppercase
-static std::string uppercase(const std::string& s) {
-    std::string upper = s;
-    for (int i = 0; i < s.length(); i++) {
-        upper[i] = std::toupper(s[i]);
-    }
-    return upper;
-}
-
-// Converts ascii hex to integer
-static std::uint16_t htoi(const std::string& hex) {
-    //std::cout << "hex number: (" << hex << ")\n";
-    
-    uint16_t intNum = 0;
-    uint16_t len = hex.length();
-    
-    for (int i = 1; i < len; i++) {
-
-        if ((hex[i] > 0x29) && (hex[i] < 0x3A)) { // 0-9
-            intNum += (hex[i] - 0x30)*pow16(len-i-1);
-            //std::cout << (int)(hex[i] - 0x30) << " x " << pow16(len-i-1) << " = " << (hex[i] - 0x30)*pow16(len-i-1) << "\n";
-        }
-        else if (hex[i] > 0x40 && hex[i] < 0x47) { //A-F
-            intNum += (hex[i] - 0x37)*pow16(len-i-1);
-        }
-    }
-    return intNum;
-}
-
-// Returns 16^exp
-static int16_t pow16 (int16_t exp) {
-    int16_t x = 1;
-    for (int i = 0; i < exp; i++) {
-        x *= 16;
-    }
-    //std::cout << "16^" << exp << " = " << x << "\n";
-    return x;
-}
-
-// Splits string into vector of strings.
-static std::vector<std::string> split(const std::string& str, char delim) {
-    std::vector<std::string> strings;
-    size_t start;
-    size_t end = 0;
-    while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
-        end = str.find(delim, start);
-        strings.push_back(str.substr(start, end - start));
-    }
-    return strings;
-}
-
-// Checks if given string is a valid label name
-static bool isValidLabel(const std::string& s) {
-    // Label must start with either '_' or letter.
-    if (((s[0] < 'A') || (s[0] > 'Z')) &&  (s[0] != '_')) return false;
-    
-    // Following characters need to be alphanumeric, '-' or '_'.
-    for (int i = 1; i < s.length()-1; i++) {
-        char c = s[i];
-        if ((c < '0') || (c > '9')) {         // Not digit
-            if ((c < 'A') || (c > 'Z')) {     // Not letter
-                if ((c != '-') || (c != '_')) // Not '-' or '_'
-                    return false;
-            }
-        }
-    }
-
-    // Must end with ':'
-    if (s[s.length()-1] != ':') return false;
-
-    return true;
-}
-
-// Checks if given string is a valid mnemonic
-static bool isValidMnemonic(const std::string& s) {
-    std::map<std::string,mnemonicData>::const_iterator it = mTable.find(s);
-
-    if (it == mTable.end()) {
-        return false;
-    }
-
-    return true;
-}
-
-// Checks if given string is a number
-static bool isNumber(const std::string& s) {
-    // Should start with '$' if hex or a digit if decimal.
-    if (((s[0] < '0') || (s[0] > '9')) && (s[0] != '$')) return false;
-
-    // All other characters 
-    for (int i = 1; i < s.length()-1; i++) {
-        if (((s[0] < '0') || (s[0] > '9')) && (s[0] != '$')) return false;
-    }
-
-    return true;
-}
